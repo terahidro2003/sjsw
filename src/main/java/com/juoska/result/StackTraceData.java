5f829beef7ffc0d108df03fd396b0c9f6ba9a69d
terahidro2003 <juozas2003@gmail.com>
2024-09-25 13:41:29 +0300

    DTO for stack trace data

---
+++ src/main/java/com/juoska/result/StackTraceData.java
@@ -0,0 +1,34 @@
+package com.juoska.result;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class StackTraceData implements Serializable {
+    private List<String> methods;
+    private Long timeTaken;
+    private Double percentageOfTotalTimeTaken;
+    private Integer totalNumberOfSamples;
+
+    public StackTraceData(List<String> methods, Long timeTaken, Double percentageOfTotalTimeTaken, Integer totalNumberOfSamples) {
+        this.methods = methods;
+        this.timeTaken = timeTaken;
+        this.percentageOfTotalTimeTaken = percentageOfTotalTimeTaken;
+        this.totalNumberOfSamples = totalNumberOfSamples;
+    }
+
+    public List<String> getMethods() {
+        return methods;
+    }
+
+    public Long getTimeTaken() {
+        return timeTaken;
+    }
+
+    public Double getPercentageOfTotalTimeTaken() {
+        return percentageOfTotalTimeTaken;
+    }
+
+    public Integer getTotalNumberOfSamples() {
+        return totalNumberOfSamples;
+    }
+}
