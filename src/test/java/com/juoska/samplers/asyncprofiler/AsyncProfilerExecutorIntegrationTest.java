3f09efdea4a864ced29318c37a071407dc994e01
terahidro2003 <juozas2003@gmail.com>
2024-10-13 21:17:23 +0300

    Add basic asprof integration test

--- src/test/java/com/juoska/samplers/asyncprofiler/AsyncProfilerExecutorIntegrationTest.java
+++ src/test/java/com/juoska/samplers/asyncprofiler/AsyncProfilerExecutorIntegrationTest.java
@@ -1,17 +1,23 @@
 package com.juoska.samplers.asyncprofiler;
 
 import com.juoska.config.Config;
+import com.juoska.result.StackTraceTreeNode;
 import com.juoska.samplers.SamplerExecutorPipeline;
 import groovy.util.logging.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class AsyncProfilerExecutorIntegrationTest {
     @Test
-    public void test() throws IOException, InterruptedException {
+    public void test() {
         // config
         Config config = new Config(
                 "./target/classes",
@@ -21,13 +27,32 @@ class AsyncProfilerExecutorIntegrationTest {
                 "./asprof.sjsw.output.test.raw.json"
         );
         Duration duration = Duration.ofSeconds(10);
-
         SamplerExecutorPipeline pipeline = new AsyncProfilerExecutor();
 
-        // run
-        pipeline.execute(config, duration);
-        pipeline.write("destination.json");
+        // run (and assert whether both phases throw an exception)
+        Assertions.assertDoesNotThrow(() -> pipeline.execute(config, duration));
+        Assertions.assertDoesNotThrow(() -> pipeline.write("destination.json"));
 
-        // assert
+        // assert that tree at least includes benchmark method names
+        Set<String> methodNames = Set.of("com.juoska.benchmark.TestBenchmark.methodB",
+                "com.juoska.benchmark.TestBenchmark.methodA", "com.juoska.benchmark.TestBenchmark.main");
+        assertThat(isTreeAssumedValid(pipeline.getStackTraceTree())).containsAnyOf(methodNames.toArray(new String[0]));
+    }
+
+    private Set<String> isTreeAssumedValid(StackTraceTreeNode root) {
+        Set<String> treeMethodNames = new HashSet<>();
+        isTreeAssumedValidRecursive(root, "", false, treeMethodNames);
+        return treeMethodNames;
+    }
+
+    private void isTreeAssumedValidRecursive(StackTraceTreeNode node, String prefix, boolean isLast, Set<String> methodNames) {
+        if (node.getMethodName() != null) {
+            methodNames.add(node.getMethodName());
+        }
+
+        List<StackTraceTreeNode> children = node.getChildren();
+        for (int i = 0; i < children.size(); i++) {
+            isTreeAssumedValidRecursive(children.get(i), prefix + (isLast ? "    " : "│   "), i == children.size() - 1, methodNames);
+        }
     }
 }
