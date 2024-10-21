9c11ffc1e1a603628a01753c87cd68641f2b39ba
Juozas Skarbalius <juozas2003@gmail.com>
2024-10-21 08:39:45 +0200

    TestBenchmark as a separate maven project in test resources dir.

--- src/test/resources/TestBenchmark/src/main/java/com/juoska/benchmark/TestBenchmark.java
+++ src/test/resources/TestBenchmark/src/main/java/com/juoska/benchmark/TestBenchmark.java
@@ -1,9 +1,5 @@
 package com.juoska.benchmark;
 
-import groovy.util.logging.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
