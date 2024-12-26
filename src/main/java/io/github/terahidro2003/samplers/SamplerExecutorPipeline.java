78e5b2c105cd5bec11afe0a5cc10a8a76b3bb589
Juozas Skarbalius <juozas2003@gmail.com>
2024-12-26 20:20:02 +0200

    Describe javaAgent method

--- src/main/java/io/github/terahidro2003/samplers/SamplerExecutorPipeline.java
+++ src/main/java/io/github/terahidro2003/samplers/SamplerExecutorPipeline.java
@@ -9,7 +9,16 @@ import java.time.Duration;
 
 public interface SamplerExecutorPipeline {
 
+    /**
+     * Generates javaagent string with configured async-profiler agent.
+     * Async-profiler executables are downloaded if not specified in the configuration variable.
+     * @param config - configuration object.
+     * @param samplingDuration - maximum sampling duration. If Java process takes less time to execute than samplingDuration
+     *                         , then the real sampling duration is equal to Java process execution time.
+     * @return object with two strings: rawOutputPath - the path of the output file, javaAgentPath - java agent with async-profiler as a string.
+     */
     MeasurementInformation javaAgent(Config config, Duration samplingDuration);
+
 
     void execute(long pid, Config config, Duration samplingDuration) throws InterruptedException, IOException;
 
