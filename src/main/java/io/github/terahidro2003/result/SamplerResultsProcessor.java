882817e9ea247e6d10140209bc6e7888e6ec960b
Juozas Skarbalius <juozas2003@gmail.com>
2024-12-27 15:32:24 +0200

    Read multiple JFRs

--- src/main/java/io/github/terahidro2003/result/SamplerResultsProcessor.java
+++ src/main/java/io/github/terahidro2003/result/SamplerResultsProcessor.java
@@ -2,6 +2,7 @@ package io.github.terahidro2003.result;
 
 import de.dagere.peass.config.MeasurementConfig;
 import de.dagere.peass.measurement.rca.data.CallTreeNode;
+import groovy.util.logging.Slf4j;
 import io.github.terahidro2003.config.Config;
 import io.github.terahidro2003.samplers.asyncprofiler.AsyncProfilerHelper;
 import io.github.terahidro2003.samplers.jfr.ExecutionSample;
@@ -10,24 +11,74 @@ import io.github.terahidro2003.utils.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import static io.github.terahidro2003.samplers.asyncprofiler.AsyncProfilerExecutor.log;
+
+@Slf4j
 public class SamplerResultsProcessor {
+
+    public List<File> listJfrMeasurementFiles(Path directory, List<String> containables) {
+        return List.of(Objects.requireNonNull(directory.toFile().listFiles((dir, name) -> isMeasurementJfr(name, containables))));
+    }
+
+    public List<File> extractSamplesFromMultipleJFRs(Path directory, Config config, String commit) throws IOException {
+        List<File> samples = new ArrayList<>();
+        List<File> jfrs = listJfrMeasurementFiles(directory, List.of(commit));
+
+        for (File file : jfrs) {
+            if (file.getName().endsWith(".jfr")) {
+                samples.add(extractSamplesFromJFR(file, "extracted_jfr_samples_" + file.getName() + ".json", config));
+            }
+        }
+        return jfrs;
+    }
+
+    private boolean isMeasurementJfr(String filename, List<String> containables) {
+        if (!filename.endsWith(".jfr")) {
+            return false;
+        }
+
+        for (String containable : containables) {
+            if (!filename.contains(containable)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     * @param jfrFile - json file containing samples derived from JFR file
+     * @return
+     */
     public List<ExecutionSample> readJfrFile(File jfrFile) {
         try {
+            log.info("Reading serialized sample json file {}", jfrFile);
             List<ExecutionSample> samples = ExecutionSample.parseJson(jfrFile.getAbsolutePath());
-
+            log.info("Loaded {} samples", samples.size());
             return samples;
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
-    public File extractSamplesFromJFR(File file, Config config) throws IOException {
+    public List<ExecutionSample> getExecutionSamplesFromMultipleJsons(List<File> serializedSamples) {
+        List<ExecutionSample> samples = new ArrayList<>();
+        log.info("Reading serialized sample json files");
+        for (File file : serializedSamples) {
+            samples.addAll(readJfrFile(file));
+        }
+        log.info("Loaded {} samples", samples.size());
+        return samples;
+    }
+
+    public File extractSamplesFromJFR(File file, String jsonFileName, Config config) throws IOException {
         // retrieve samples from JFR file
         List<String> command = new ArrayList<>();
         command.add("jfr");
@@ -42,7 +93,7 @@ public class SamplerResultsProcessor {
         ProcessBuilder processBuilder = new ProcessBuilder(command);
         Process process = processBuilder.start();
         InputStream processInputStream = process.getInputStream();
-        String jfr_json = AsyncProfilerHelper.getInstance(config).rawProfilerOutput("parsed_jfr_samples" + System.currentTimeMillis() + ".json").getAbsolutePath();
+        String jfr_json = AsyncProfilerHelper.getInstance(config).rawProfilerOutput(jsonFileName).getAbsolutePath();
         FileUtils.inputStreamToFile(processInputStream, jfr_json);
         return new File(jfr_json);
     }
