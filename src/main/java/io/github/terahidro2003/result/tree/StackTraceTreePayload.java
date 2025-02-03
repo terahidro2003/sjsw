778454ae439b9296510b78cf4df6b6496464c274
terahidro2003 <juozas2003@gmail.com>
2025-02-03 02:24:01 +0200

    Remove unused dependencies

--- src/main/java/io/github/terahidro2003/result/tree/StackTraceTreePayload.java
+++ src/main/java/io/github/terahidro2003/result/tree/StackTraceTreePayload.java
@@ -1,10 +1,5 @@
 package io.github.terahidro2003.result.tree;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class StackTraceTreePayload {
     private String methodName;
 
