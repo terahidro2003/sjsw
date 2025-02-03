132ff54d21977660348bbd3838d148bd9c2e9ef3
terahidro2003 <juozas2003@gmail.com>
2025-02-03 17:22:52 +0200

    Add test with 5 VMs from real Peass measurement

--- src/test/java/io/github/terahidro2003/samplers/asyncprofiler/TreeBuildingTest.java
+++ src/test/java/io/github/terahidro2003/samplers/asyncprofiler/TreeBuildingTest.java
@@ -14,7 +14,7 @@ public class TreeBuildingTest {
 
     @Test
     public void test() {
-        String testcase = "testing";
+        String testcase = "testing()";
         List<File> jfrs = List.of(
                 new File(resourcesDir + "/1.jfr"),
                 new File(resourcesDir + "/2.jfr")
@@ -24,7 +24,11 @@ public class TreeBuildingTest {
         StackTraceTreeNode bat = processor.getTreeFromJfr(jfrs);
 
         StackTraceTreeBuilder builder = new StackTraceTreeBuilder();
+
+        // retrieves subtrees that share the same parent node
         List<StackTraceTreeNode> filteredSubtrees = builder.filterMultiple(testcase, bat, false);
+
+        // filters out common JVM and native method call nodes from all retrieved subtrees
         filteredSubtrees = filteredSubtrees.stream().map(builder::filterJvmNodes).toList();
         filteredSubtrees.forEach(tree -> {
             System.out.println();
@@ -42,4 +46,49 @@ public class TreeBuildingTest {
         System.out.println();
         mergedTree.printTree();
     }
+
+    @Test
+    public void testMoreVms() {
+        String testcase = "testMe()";
+        List<File> jfrs = new ArrayList<>();
+        for (int i = 3; i < 8; i++) {
+            jfrs.add(new File(resourcesDir + "/" + i + ".jfr"));
+        }
+
+        SamplerResultsProcessor processor = new SamplerResultsProcessor();
+        StackTraceTreeBuilder builder = new StackTraceTreeBuilder();
+        List<StackTraceTreeNode> vmTrees = new LinkedList<>();
+        for (int i = 0; i<5; i++) {
+            StackTraceTreeNode vmTree = buildVmTree(jfrs.get(i), processor, testcase);
+            vmTrees.add(vmTree);
+        }
+
+        // filters out common JVM and native method call nodes from all retrieved subtrees
+        vmTrees = vmTrees.stream().map(builder::filterJvmNodes).toList();
+        vmTrees.forEach(tree -> {
+            System.out.println();
+            tree.printTree();
+            System.out.println();
+        });
+
+        Map<List<String>, List<Double>> measurementsMap = builder.createMeasurementsMap(vmTrees, testcase);
+        StackTraceTreeNode mergedTree = StackTraceTreeBuilder.mergeTrees(vmTrees);
+
+        builder.addLocalMeasurements(mergedTree, measurementsMap, "11111");
+
+        System.out.println();
+        System.out.println();
+        mergedTree.printTree();
+    }
+
+    public StackTraceTreeNode buildVmTree(File jfr, SamplerResultsProcessor processor, String testcase) {
+        StackTraceTreeNode bat = processor.getTreeFromJfr(List.of(jfr));
+
+        StackTraceTreeBuilder builder = new StackTraceTreeBuilder();
+
+        // retrieves subtrees that share the same parent node
+        List<StackTraceTreeNode> filteredSubtrees = builder.filterMultiple(testcase, bat, false);
+        return StackTraceTreeBuilder.mergeTrees(filteredSubtrees);
+    }
+
 }
